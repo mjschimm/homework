7466008f4711877248a956ad0a53ad0ec8a2b197
mjschimm <mjschimm@gmail.com>
2019-08-05 20:25:11 -0700

    Button stylization in modal

--- static/js/home.js
+++ static/js/home.js
@@ -1,13 +1,31 @@
+// import "http://code.jquery.com/color/jquery.color-2.1.2.js";
+
 $('#New').click(function() {
-    // alert("fuck")
-    // console.log($(this));
     $('.modal').addClass('modal-show');
     $('.modal-content').addClass('modal-content-drop');
 });
 
 $('#Cancel').click(function() {
-    // alert("fuck")
-    // console.log($(this));
     $('.modal').removeClass('modal-show');
     $('.modal-content').removeClass('modal-content-drop');
 });
+
+$('.modal button').hover(
+    function() {
+        $(this).animate({
+            'background-position-x': "0%",
+            color: "#ffffff"
+        }, {
+            duration: 250,
+            queue: false
+        });
+    }, function() {
+        $(this).animate({
+            'background-position-x': "99%",
+            'color': '#666'
+        }, {
+            duration: 250,
+            queue: false
+        });
+    }
+);
